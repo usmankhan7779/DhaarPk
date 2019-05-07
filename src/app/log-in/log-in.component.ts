99e8a8a2e1042c7d5855fb46b4ceebe30d4720d1
usmankhanbrainplow <usman.khan@brainplow.com>
2019-05-07 15:20:27 +0500

    nvvnvn

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -5,12 +5,10 @@ import { LoginService } from './log-in.services';
 import {FormControl, NgModel, Validators, FormGroup} from '@angular/forms';
 import {NgForm} from '@angular/forms';
 import {HttpService} from '../services/http-service';
-import { Headers} from "@angular/http";
+import {Headers, Response} from '@angular/http';
 import { RecapchaComponent } from '../recapcha/recapcha.component';
 import { RecapchaService } from '../recapcha/recapcha.service';
-import { AuthService } from 'angular4-social-login';
-import { SocialUser } from 'angular4-social-login';
-import { GoogleLoginProvider, FacebookLoginProvider } from 'angular4-social-login';
+import { GoogleLoginProvider,AuthService,SocialUser, FacebookLoginProvider } from 'angular4-social-login';
 import Swal from 'sweetalert2';
 declare const $: any;
 
@@ -31,6 +29,11 @@ export class LogInComponent implements OnInit {
   login_error = false;
   Waitcall = false;
   logout: string;
+  fb_id: any;
+  fb_name: any;
+  fb_email: any;
+  fb_photo_Url: any;
+  fb_token: any;
   ProID: string;
   RedirectFromlogin: string;
   CatName: string;
@@ -44,69 +47,70 @@ export class LogInComponent implements OnInit {
   // );
 
 
-
-
-  constructor( @Inject(PLATFORM_ID) private platformId: Object,
-               private http: HttpService,
-               private recha: RecapchaService,
-               private authService: AuthService,
-               private obj: LoginService,
-               private _nav: Router,
-               private route: ActivatedRoute) { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object,
+              private http: HttpService,
+              private recha: RecapchaService,
+              private authService: AuthService,
+              private obj: LoginService,
+              private _nav: Router,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
-    $(".mat-input").focus(function(){
+    $(".mat-input").focus(function () {
       $(this).parent().addClass("is-active is-completed");
     });
-    
-    $(".mat-input").focusout(function(){
-      if($(this).val() === "")
+
+    $(".mat-input").focusout(function () {
+      if ($(this).val() === "")
         $(this).parent().removeClass("is-completed");
       $(this).parent().removeClass("is-active");
     })
-    if (isPlatformBrowser(this.platformId)){
-      if(localStorage.getItem('UserID')){
-        this._nav.navigate(['/']);
+    if (isPlatformBrowser(this.platformId)) {
 
-    } else {
-        this.authService.authState.subscribe((user) => {
-          this.user = user;
-          console.log('Name of user', this.user)
+      //   if(localStorage.getItem('UserID'))
+      //   {
+      //     this._nav.navigate(['/']);
+      //
+      // } else {
+      // this.authService.authState.subscribe((user) => {
+      //   this.user = user;
+      //   console.log('Name of user', this.user);
+      // });
+      this.sub = this.route
+        .queryParams
+        .subscribe(params => {
+          // Defaults to 0 if no query param provided.
+          this.logout = params['Logout'] || 0;
+          this.CatName = params['CatName'] || null;
+          this.ProID = params['ProID'] || null;
+          this.checkout = params['checkout'] || null;
+
         });
-        this.sub = this.route
-          .queryParams
-          .subscribe(params => {
-            // Defaults to 0 if no query param provided.
-            this.logout = params['Logout'] || 0 ;
-            this.CatName = params['CatName'] || null ;
-            this.ProID = params['ProID'] || null ;
-            this.checkout = params['checkout'] || null ;
-
-          });
 
 
+      if (localStorage.getItem('Reg') === 'Done') {
 
-        if ( localStorage.getItem('Reg') === 'Done') {
-
-          this.SignUpDOne = true;
-          localStorage.setItem('Reg', null);
-        }
-        if ( localStorage.getItem('StoreReg') === 'Done') {
-
-          this.StoreUpDOne = true;
-          localStorage.setItem('StoreReg', null);
-        }
-        window.scrollTo(0, 0);
-
-        if (this.logout === 'yes') {
-          this.LogOutClick();
-
-        }
-        // get return url from route parameters or default to '/'
-        // this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/profile';
+        this.SignUpDOne = true;
+        localStorage.setItem('Reg', null);
       }
+      if (localStorage.getItem('StoreReg') === 'Done') {
+
+        this.StoreUpDOne = true;
+        localStorage.setItem('StoreReg', null);
+      }
+      window.scrollTo(0, 0);
+
+      if (this.logout === 'yes') {
+        this.LogOutClick();
+
+      }
+      // get return url from route parameters or default to '/'
+      // this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/profile';
     }
   }
+
+
   
   signInWithGoogle(): void {
     this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
@@ -114,7 +118,40 @@ export class LogInComponent implements OnInit {
   }
 
   signInWithFB(): void {
-    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID)
+    //   (userData) => {
+    //   this.user = userData;
+    //   console.log('User Data...', this.user);
+    // });
+      .then(this.socialCallBack).catch(user => console.log(user));
+  }
+
+
+  socialCallBack = (user) => {
+    this.user = user;
+    console.log(this.user);
+    const headers = { 'Content-Type': 'application/json' };
+    return  this.http.post('https://apis.dhaar.pk/user/sociallogin/', {
+      id: this.fb_id,
+      name: this.fb_name,
+      email: this.fb_email,
+      photoUrl: this.fb_photo_Url,
+      authToken: this.fb_token
+    }).map((res: Response) => res.json()).subscribe(data => {
+          // let user = {
+          //   user_id: this.jwtHelper.decodeToken(data['token']).user_id,
+          //   username: this.jwtHelper.decodeToken(data['token']).username,
+          //   token: data['token'] };
+          // if (user && user.token) {
+          //   localStorage.setItem('loged_in', '1');
+          //   localStorage.setItem('currentUser', JSON.stringify(user));
+          //   localStorage.setItem('profilePhoto' , this.pic);
+            this._nav.navigate(['/dashboard']);
+          //   this.showSuccess();
+          // }
+      console.log(data);
+        }
+      );
   }
 
   signOut(): void {
