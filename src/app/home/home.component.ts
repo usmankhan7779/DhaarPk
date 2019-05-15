343dce39487ecc43d1465ba2f84fe6da60a15fa2
Haseeb6776 <41138641+Haseeb6776@users.noreply.github.com>
2019-05-15 15:01:06 +0500

    15-05-19

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -533,11 +533,11 @@ export class HomeComponent implements OnInit {
         setTimeout(function () {
           $('.homeSlider').slick({
             infinite: true,
-            slidesToShow: 5,
+            slidesToShow: 6,
             slidesToScroll: 1,
             autoplay: false,
-            prevArrow: '<button class="leftRs" style="left: 30px;"><i class="fa fa-angle-left"></i></button>',
-            nextArrow: '<button class="rightRs" style="right: 30px;"><i class="fa fa-angle-right"></i></button>',
+            prevArrow: '<button class="leftRs" style="left: -27px;"><i class="fa fa-angle-left"></i></button>',
+            nextArrow: '<button class="rightRs" style="right: -27px;"><i class="fa fa-angle-right"></i></button>',
 
             responsive: [
               {
