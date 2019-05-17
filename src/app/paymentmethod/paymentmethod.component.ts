785bfc61c468006c8ee46a846fa891210dba1fdf
Haseeb6776 <41138641+Haseeb6776@users.noreply.github.com>
2019-05-17 17:18:24 +0500

    17-5-19

---
+++ src/app/paymentmethod/paymentmethod.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paymentmethod',
+  templateUrl: './paymentmethod.component.html',
+  styleUrls: ['./paymentmethod.component.scss']
+})
+export class PaymentmethodComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
