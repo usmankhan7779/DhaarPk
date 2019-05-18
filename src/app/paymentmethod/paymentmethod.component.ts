06db3d35405571afa9428401fc8efe84d352948d
Haseeb6776 <41138641+Haseeb6776@users.noreply.github.com>
2019-05-18 13:37:53 +0500

    18-0-19

--- src/app/paymentmethod/paymentmethod.component.ts
+++ src/app/paymentmethod/paymentmethod.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit, NgModule } from '@angular/core';
+import { Component, OnInit,NgModule, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { LoginService } from '../log-in/log-in.services';
+import { FormBuilder, Validators, NgControl, RadioControlValueAccessor, FormControl, FormGroup } from '@angular/forms'
+ 
+import Swal from 'sweetalert2';
+import { UploadItemService } from '../file-uploads/upload-item-service';
+import { Observable } from 'rxjs';
+declare const $: any;
+export interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-paymentmethod',
@@ -6,10 +20,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paymentmethod.component.scss']
 })
 export class PaymentmethodComponent implements OnInit {
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 
-  constructor() { }
-
+  constructor(private fb: FormBuilder) {  }
+  signupForm: FormGroup;
   ngOnInit() {
+    this.signupForm = this.fb.group({
+
+      'FName': ['']
+       
+    })
   }
 
 }
